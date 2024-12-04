8966a9f584cdb9bd57d1db1f91d281f17ab48c61
Sara Bohannon <sarandipity91@gmail.com>
2024-12-03 18:16:16 -0600

    whereAmI

--- inheritance/testing-inheritance/exercises/technology/src/main/java/org/launchcode/Computer.java
+++ inheritance/testing-inheritance/exercises/technology/src/main/java/org/launchcode/Computer.java
@@ -5,11 +5,20 @@ public class Computer {
     private String model;
     private boolean needsCharge;
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public Computer(String brand, String model, boolean needsCharge) {
         super();
         this.brand = brand;
         this.model = model;
         this.needsCharge = needsCharge;
+
     }
 }
 

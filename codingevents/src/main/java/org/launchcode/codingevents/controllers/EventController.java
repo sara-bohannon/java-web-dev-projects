c8d81604378b72f2a9423a10f0dee220b8bc6669
Sara Bohannon <sarandipity91@gmail.com>
2024-12-05 17:04:35 -0600

    itsThymeToCry

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,10 +24,9 @@ public class EventController {
         model.addAttribute("events", events);
         return "events/index";
     }
-
-    //lives at /events/create
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
     }
+
 }
